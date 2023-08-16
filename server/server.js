93d93390327822cb0b1b93ff4f63c0a6291c6710
Jared <weissjmw@gmail.com>
2023-08-16 15:00:24 +0300

    pg.pool instead of client

--- server/server.js
+++ server/server.js
@@ -3,28 +3,28 @@ import http from "http";
 import pg from "pg";
 import "dotenv/config";
 
+let dbConfig = process.env.PGUSER
+  ? {
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDB,
+      password: process.env.PGPWD,
+      port: process.env.PGPORT,
+    }
+  : {
+      connectionString:
+        "postgres://admin:sOdTEpXhj9zKtcFc5EYdrWAB9fc2KsWz@dpg-cisl9l95rnujejq1v7gg-a/kanban_clone",
+    };
+
+const pool = new pg.Pool(dbConfig);
+
 async function queryDb(query) {
-  let client;
   try {
-    client = new pg.Client(
-      process.env.PGUSER
-        ? {
-            user: process.env.PGUSER,
-            host: process.env.PGHOST,
-            database: process.env.PGDB,
-            password: process.env.PGPWD,
-            port: process.env.PGPORT,
-          }
-        : "postgres://admin:sOdTEpXhj9zKtcFc5EYdrWAB9fc2KsWz@dpg-cisl9l95rnujejq1v7gg-a/kanban_clone"
-    );
-    await client.connect();
-    const res = await client.query(query);
+    const res = await pool.query(query);
     return res.rows;
   } catch (err) {
     console.error(err);
     return [];
-  } finally {
-    await client.end();
   }
 }
 
@@ -47,7 +47,7 @@ const server = http.createServer(async (req, res) => {
       })
     );
   } else if (req.url === "/boards") {
-    let data = await queryDb("select * from board;");
+    let data = await queryDb("select * from boards;");
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
     res.write(JSON.stringify(data));
@@ -81,5 +81,7 @@ const server = http.createServer(async (req, res) => {
 });
 
 server.listen(5030, () => {
-  console.log("listening for requests on port 5030");
+  console.log(
+    "listening for requests on port 5030, url is http://127.0.0.1:5030"
+  );
 });
